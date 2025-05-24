56ab983b634d5aa6b8535f49d56ee7f43a55f503
iamlive <86843363+Swapnayg@users.noreply.github.com>
2025-05-24 19:10:58 +0530

    updated with the footer

--- src/masterLayout/MasterLayout.jsx
+++ src/masterLayout/MasterLayout.jsx
@@ -1034,11 +1034,11 @@ const handleLogOut = (e) => {
         <footer className="d-footer">
           <div className="row align-items-center justify-content-between">
             <div className="col-auto">
-              <p className="mb-0">© 2025 WowDash. All Rights Reserved.</p>
+              <p className="mb-0"></p>
             </div>
             <div className="col-auto">
               <p className="mb-0">
-                Made by <span className="text-primary-600">wowtheme7</span>
+                <span className="text-primary-600"></span>
               </p>
             </div>
           </div>
